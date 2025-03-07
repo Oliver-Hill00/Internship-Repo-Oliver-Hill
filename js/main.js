3d7d3848af03908df0518800e26a3bc5e36880c9
Oliver Hill <Oliverhill@u.boisestate.edu>
2025-03-06 18:02:31 -0700

    First commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,57 @@
 let timerId = null;
+let moving = false;
 
 window.addEventListener("DOMContentLoaded", function() {
    document.addEventListener("click", startAnimation);
 });
 
-function startAnimation(e) {
-   //TODO
+function startAnimation(e)
+{
+   if(timerId == null)
+   {
+      clearInterval(timerId);
+   }
+
+    //clicked position
+   let clickPoint_x = e.clientX;
+   let clickPoint_y = e.clientY;
+   
+   const img = document.querySelector("img");
+
+   let imgWidth = img.offsetWidth;
+   let imgHeight = img.offsetHeight;
+
+   let targetX = clickPoint_x - imgWidth / 2;
+   let targetY = clickPoint_y - imgHeight / 2;
+
+   //start new timer
+   timerId = setInterval(() => moveImage(targetX, targetY), 10);
+
 }
 
-function moveImage(x, y) {
-   //TODO
+function moveImage(targetX, targetY)
+{
+   const img = document.querySelector("img");
+
+   // Determine location of image
+   let imgX = parseInt(img.style.left);
+   let imgY = parseInt(img.style.top);
+
+   //determine movement direction of image when activated
+   let movement_X = targetX > imgX ? 1 : targetX < imgX ? -1 : 0;
+   let movement_Y = targetY > imgY ? 1 : targetY < imgY ? -1 : 0;
+
+   //move image by 1 pixel per call
+   img.style.left - (imgX + movement_X) + "px";
+   img.style.top = (imgY + movement_Y) + "px";
+
+
+   if(imgX  === targetX && imgY === targetY)
+   {
+      //stop animation when image reaches designated position
+      clearInterval(timerId);
+      timerId = null;
+   }
+
+
 }
