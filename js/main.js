4b13e9064af7a1ecc48e483f8e8c3d7906317333
Oliver Hill <Oliverhill@u.boisestate.edu>
2025-03-06 18:05:49 -0700

    Skull moves, bounces wrong. 3/6/25

--- js/main.js
+++ js/main.js
@@ -1,57 +1,51 @@
 let timerId = null;
-let moving = false;
 
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
    document.addEventListener("click", startAnimation);
 });
 
-function startAnimation(e)
-{
-   if(timerId == null)
-   {
+function startAnimation(e) {
+   // Stop any previous animation if it's running
+   if (timerId !== null) {
       clearInterval(timerId);
    }
 
-    //clicked position
+   // Get the clicked position
    let clickPoint_x = e.clientX;
    let clickPoint_y = e.clientY;
-   
+
    const img = document.querySelector("img");
 
+   // Get image dimensions for centering
    let imgWidth = img.offsetWidth;
    let imgHeight = img.offsetHeight;
 
+   // Set target position (centered on click)
    let targetX = clickPoint_x - imgWidth / 2;
    let targetY = clickPoint_y - imgHeight / 2;
 
-   //start new timer
+   // Start a new animation timer
    timerId = setInterval(() => moveImage(targetX, targetY), 10);
-
 }
 
-function moveImage(targetX, targetY)
-{
+function moveImage(targetX, targetY) {
    const img = document.querySelector("img");
 
-   // Determine location of image
+   // Determine current position
    let imgX = parseInt(img.style.left);
    let imgY = parseInt(img.style.top);
 
-   //determine movement direction of image when activated
+   // Determine movement direction
    let movement_X = targetX > imgX ? 1 : targetX < imgX ? -1 : 0;
    let movement_Y = targetY > imgY ? 1 : targetY < imgY ? -1 : 0;
 
-   //move image by 1 pixel per call
-   img.style.left - (imgX + movement_X) + "px";
+   // Move image by 1 pixel per step
+   img.style.left = (imgX + movement_X) + "px";
    img.style.top = (imgY + movement_Y) + "px";
 
-
-   if(imgX  === targetX && imgY === targetY)
-   {
-      //stop animation when image reaches designated position
+   // Stop animation when image reaches target
+   if (imgX === targetX && imgY === targetY) {
       clearInterval(timerId);
       timerId = null;
    }
-
-
 }
